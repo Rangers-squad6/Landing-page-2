5adc1df93cc349b16e426d137ccad98f1eb3f17c
allankovac <allankfrasco@gmail.com>
2022-10-12 16:12:20 -0300

    Adicionando o bootstrap para demonstração de como usar o git

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 function App() {
   return (
     <div className="App">
-     <h1>Teste</h1>
+      <h1>Teste</h1>
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary">Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+    </Modal.Dialog>
     </div>
   );
 }
